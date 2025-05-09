9fe9de06caa35626e283212f02182edea46ad8d1
l0o0 <linxzh1989@gmail.com>
2025-05-09 21:55:06 +0800

    add: default prefix is v1

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 
 # Include the routers
-app.include_router(users.router)
+app.include_router(users.router, prefix="/v1")
 # app.include_router(auth.router, prefix="/auth")
-app.include_router(journals.router)
+app.include_router(journals.router, prefix="/v1")
